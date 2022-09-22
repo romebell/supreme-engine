4f901aa8e219e2a8645d08897d2ab249189e5494
Rome Bell <romebell5@gmail.com>
2022-09-21 23:40:01 -0700

    chore: test genius api

---
+++ apiTest.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const axios = require('axios');
+
+const options = {
+    method: 'GET',
+    url: 'https://genius.p.rapidapi.com/search',
+    params: {q: 'drake'},
+    headers: {
+      'X-RapidAPI-Key': process.env.API_KEY,
+      'X-RapidAPI-Host': 'genius.p.rapidapi.com'
+    }
+};
+axios.request(options).then((response) => {
+    console.log('*********connected to API***********')
+    console.log('this is array' , response.data.response.hits);
+}).catch((error) => {
+    console.error('There was an error retrieving response from API', error);
+});
